721fa8690e4bc98041c80eca14c998758f801bad
JYtorch <87455861+JYtorch@users.noreply.github.com>
2022-06-23 03:25:20 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js")
 importScripts("https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js")
 
 firebase.initializeApp({
-  apiKey: `${ secrets.API_KEY }`,
-  authDomain: `${ secrets.AUTH_DOMAIN }`,
-  projectId: `${ secrets.PROJECT_ID }`,
-  storageBucket: `${ secrets.STORAGE_BUCKET }`,
-  messagingSenderId: `${ secrets.MESSAGING_SENDER_ID }`,
-  appId: `${ secrets.APP_ID }`,
-  measurementId: `${ secrets.MEASUREMENT_ID }`,     
+  apiKey: '${{ secrets.API_KEY }}',
+  authDomain: '${{ secrets.AUTH_DOMAIN }}',
+  projectId: '${{ secrets.PROJECT_ID }}',
+  storageBucket: '${{ secrets.STORAGE_BUCKET }}',
+  messagingSenderId: '${{ secrets.MESSAGING_SENDER_ID }}',
+  appId: '${{ secrets.APP_ID }}',
+  measurementId: '${{ secrets.MEASUREMENT_ID }}',   
 });
 const messaging = firebase.messaging();
 // console.log(messaging)
