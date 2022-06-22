7f1bc50bf5291f8e2ef903caaa9ca36bab2c15f2
JYtorch <87455861+JYtorch@users.noreply.github.com>
2022-06-23 03:23:06 +0900

    Update 10000hours.js

--- js/10000hours.js
+++ js/10000hours.js
@@ -73,13 +73,13 @@ closeButton.addEventListener("click", closeModal);
 startButton.addEventListener("click", calculator);
 
 const config = {
-  apiKey: `${ secrets.API_KEY }`,
-  authDomain: `${ secrets.AUTH_DOMAIN }`,
-  projectId: `${ secrets.PROJECT_ID }`,
-  storageBucket: `${ secrets.STORAGE_BUCKET }`,
-  messagingSenderId: `${ secrets.MESSAGING_SENDER_ID }`,
-  appId: `${ secrets.APP_ID }`,
-  measurementId: `${ secrets.MEASUREMENT_ID }`,
+  apiKey: '${{ secrets.API_KEY }}',
+  authDomain: '${{ secrets.AUTH_DOMAIN }}',
+  projectId: '${{ secrets.PROJECT_ID }}',
+  storageBucket: '${{ secrets.STORAGE_BUCKET }}',
+  messagingSenderId: '${{ secrets.MESSAGING_SENDER_ID }}',
+  appId: '${{ secrets.APP_ID }}',
+  measurementId: '${{ secrets.MEASUREMENT_ID }}',
 };
 
 firebase.initializeApp(config);
